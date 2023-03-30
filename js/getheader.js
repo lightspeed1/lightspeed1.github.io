7cf0d225c2c41e1268c1be88e07fb2e5d565cbef
Andrew Strong <strong.andrew.j@gmail.com>
2023-03-30 10:51:23 -0600

    finshed header and education page

---
+++ js/getheader.js
@@ -0,0 +1,10 @@
+fetch('headcontent.html')
+    .then(response => response.text())
+        .then(html => {
+            document.getElementById('head').innerHTML += html;
+        });
+fetch('bodyheader.html')
+    .then(response => response.text())
+        .then(html => {
+            document.getElementById('header-placeholder').innerHTML += html;
+        });
